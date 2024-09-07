5410742d2dd91cf53ed1f8d14d143b20e3d27e4c
Kirill <iyaargg@gmail.com>
2024-09-07 19:09:16 +0500

    Remove player cached data when he is quit.

--- src/main/java/me/rejomy/levelsystem/listener/ConnectionListener.java
+++ src/main/java/me/rejomy/levelsystem/listener/ConnectionListener.java
@@ -5,6 +5,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class ConnectionListener implements Listener {
     private final DataManager dataManager;
@@ -16,5 +17,11 @@ public class ConnectionListener implements Listener {
     @EventHandler(priority = EventPriority.MONITOR)
     public void onJoin(PlayerJoinEvent event) {
         dataManager.add(event.getPlayer());
+    }
+
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onQuit(PlayerQuitEvent event) {
+        dataManager.remove(event.getPlayer());
     }
 }
